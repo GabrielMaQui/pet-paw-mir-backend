b76189160ee5c6e774cb9e63f67020c4fbba1656
WLupacaTicona <135378021+PunoBootcamper@users.noreply.github.com>
2024-10-16 07:51:49 -0500

    Refactor/config william lupaca (#6)
    * chore: update users table
    
    * refactor: change names

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
-import {getAllUsersHandler, getOneUserHandler} from './user.controller';
+import {
+  createUserHandler,
+  getAllUsersHandler,
+  getOneUserHandler,
+} from './user.controller';
 
 //Manejador de rutas
 const router = Router();
 
-
 //Lista de rutas para un modelo (tabla)
 router.get('/', getAllUsersHandler);
+router.post('/', createUserHandler);
 router.patch('/:id', getOneUserHandler);
-
 
 //exportar la aplicacion
 export default router;
