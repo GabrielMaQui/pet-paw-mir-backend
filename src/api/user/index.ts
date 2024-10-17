d324baa9a67d1ad3f46bc29deac8addf0e19b46e
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-10-16 22:58:02 -0500

    feat: add implementation for prisma of getAll and create user

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
-import {
-  createUserHandler,
-  getAllUsersHandler,
-  getOneUserHandler,
-} from './user.controller';
+import { createUserHandler, getAllUsersHandler } from './user.controller';
 
 //Manejador de rutas
 const router = Router();
@@ -11,7 +7,6 @@ const router = Router();
 //Lista de rutas para un modelo (tabla)
 router.get('/', getAllUsersHandler);
 router.post('/', createUserHandler);
-router.patch('/:id', getOneUserHandler);
 
 //exportar la aplicacion
 export default router;
