51aea16a5d48115966dabb1fdab49f46578983ef
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-10-22 00:04:15 -0500

    feat: complete CRUD documentation API User

--- src/api/post/index.ts
+++ src/api/post/index.ts
@@ -23,7 +23,7 @@ router.patch('/:id', updatePostHandler);
 export default router;
 
 /*
-localhost:3000/api/posts                GET   Get all posts       
+localhost:3000/api/posts                GET   Get all posts
 localhost:3000/api/posts/myposts        GET   Get all posts of the authenticated user
 localhost:3000/api/posts/user/:userId   GET   Get all posts of a user
 localhost:3000/api/posts/:id            GET   Get a post by id
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -23,9 +23,115 @@ const router = Router();
  */
 router.get('/', getAllUsersHandler);
 
+// Crear un nuevo usuario
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Crea un nuevo usuario
+ *     tags: [Usuarios]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Nombre de usuario único.
+ *               password:
+ *                 type: string
+ *                 description: Contraseña del usuario.
+ *               email:
+ *                 type: string
+ *                 description: Correo electrónico del usuario.
+ *               role:
+ *                 type: string
+ *                 enum: [USER, ADMINISTRADOR]
+ *                 description: Rol del usuario en el sistema.
+ *             required:
+ *               - username
+ *               - password
+ *               - email
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente.
+ *       400:
+ *         description: Error en la creación del usuario.
+ */
 router.post('/', createUserHandler);
-//router.get('/:id', getOneUserHandler);
+
+// Actualizar un usuario
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   patch:
+ *     summary: Actualiza un usuario existente
+ *     tags: [Usuarios]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del usuario a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Nombre de usuario (opcional).
+ *               password:
+ *                 type: string
+ *                 description: Contraseña nueva (opcional).
+ *               email:
+ *                 type: string
+ *                 description: Correo electrónico nuevo (opcional).
+ *               role:
+ *                 type: string
+ *                 enum: [USER, ADMINISTRADOR]
+ *                 description: Rol actualizado del usuario (opcional).
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado exitosamente.
+ *       404:
+ *         description: Usuario no encontrado.
+ *       403:
+ *         description: No autorizado.
+ */
 router.patch('/:id', hasRole(['ADMINISTRADOR']), updateUserHandler);
+
+//Eliminar usuario
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Elimina un usuario
+ *     tags: [Usuarios]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del usuario a eliminar
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado exitosamente.
+ *       404:
+ *         description: Usuario no encontrado.
+ *       403:
+ *         description: No autorizado.
+ */
 router.delete('/:id', hasRole(['ADMINISTRADOR']), deleteUserHandler);
 
 //exportar la aplicacion
