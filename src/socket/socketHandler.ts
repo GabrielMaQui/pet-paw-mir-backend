cbe9494dd5d017f474c05e232e104e1c4875ea81
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-12 00:05:59 -0500

    feat: add controller, model and service for chat and refactor socket-id with model user

--- src/socket/socketHandler.ts
+++ src/socket/socketHandler.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import type { Server } from 'socket.io';
-import { handleNewComment } from '../api/comment/comment.controller';
+
+const prisma = new PrismaClient();
 
 const prisma = new PrismaClient();
 
@@ -54,12 +55,75 @@ const socketHandler = (io: Server) => {
       }
     });
 
-    socket.on('newComment', async (commentData) => {
-      await handleNewComment(socket, commentData);
+    // Registro del usuario usando su User.id
+    socket.on('register', async (userId) => {
+      // Comprobamos si el usuario existe en la base de datos
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (user) {
+        await prisma.user.update({
+          where: { id: userId },
+          data: { socketId: socket.id },
+        });
+        socket.emit('login');
+      } else {
+        // Si el usuario no existe, emitir un error o crear uno nuevo
+        socket.emit('userNotFound', 'El usuario no existe.');
+        console.log('Usuario no encontrado.');
+      }
+
+      // Emitir la lista de usuarios activos
+      const activeUsers = await prisma.user.findMany({
+        where: { socketId: { not: null } },
+      });
+      io.emit('activeSessions', activeUsers);
     });
 
-    socket.on('disconnect', () => {
+    // Desconexión y actualización de socketId
+    socket.on('disconnect', async () => {
       console.log('User disconnected:', socket.id);
+
+      // Limpiamos el socketId del usuario en la base de datos cuando se desconecta
+      await prisma.user.updateMany({
+        where: { socketId: socket.id },
+        data: { socketId: null },
+      });
+
+      // Emitir la lista de usuarios activos
+      const activeUsers = await prisma.user.findMany({
+        where: { socketId: { not: null } },
+      });
+      io.emit('activeSessions', activeUsers);
+    });
+
+    // Enviar mensajes a todos los usuarios conectados
+    socket.on('sendMessage', ({ message }) => {
+      io.emit('sendMessage', { message, user: socket.id });
+    });
+
+    // Enviar mensajes privados
+    socket.on('sendMessagesPrivate', async ({ message, recipientUserId }) => {
+      // Buscar al destinatario por su User.id
+      const recipientUser = await prisma.user.findUnique({
+        where: { id: recipientUserId },
+      });
+
+      if (recipientUser?.socketId) {
+        // Enviar mensaje al destinatario si está conectado
+        io.to(recipientUser.socketId).emit('sendMessage', {
+          message,
+          user: socket.id,
+        });
+        io.to(socket.id).emit('sendMessage', { message, user: socket.id });
+      } else {
+        // Si el destinatario no está conectado
+        io.to(socket.id).emit('sendMessage', { message, user: socket.id });
+        console.log(
+          'El usuario al que intentas enviar el mensaje no está conectado.',
+        );
+      }
     });
 
     // Manejo de mensajes privados
