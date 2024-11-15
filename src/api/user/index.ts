a134e391df1afc38eab9661d09ec1472b850a1c3
WLupacaTicona <135378021+PunoBootcamper@users.noreply.github.com>
2024-11-15 16:36:31 -0500

    Feat/admin page (#23)
    * fix: modify type for post
    
    * feat: add user get id
    
    * feat: modify get post
    
    * feat: get current user by token
    
    * feat: favorites
    
    * feat: add create and read comments for post
    
    * feat: add ADMINISTRATOR on updateUserHandler
    
    * fix: correct server and post user
    
    ---------
    
    Co-authored-by: Gabriel Steven Machicao Quispe <legard2911@gmail.com>
    Co-authored-by: David Paredes <d.paredes@didepti.com>

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -107,7 +107,7 @@ router.post('/', createUserHandler);
  *       403:
  *         description: No autorizado.
  */
-router.patch('/:id', hasRole(['USER']), updateUserHandler);
+router.patch('/:id', hasRole(['USER', 'ADMINISTRATOR']), updateUserHandler);
 
 //Eliminar usuario
 /**
