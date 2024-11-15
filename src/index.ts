65c4cfcefc7406afd677e80f88ba11f36959b107
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-15 17:10:18 -0500

    fix: correct server

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,3 @@ const PORT = process.env.PORT ?? 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port  ${PORT}`);
 });
-
-server.listen(8090, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
-});
